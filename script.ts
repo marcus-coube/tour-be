d3351f14c7c314a1312e78e75eb34b0074074a37
Marcus Coube <marcus.coube@gmail.com>
2025-06-28 03:30:41 -0300

    wip backend - create partners, address and vouchers table

--- script.ts
+++ script.ts
@@ -1,10 +1,33 @@
-import {PrismaClient} from "@prisma/client";
+import {PrismaClient, partners, partner_address} from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.findMany();
-    console.log(user)
+    // const user = await prisma.user.findMany();
+    // console.log(user)
+
+
+    const partnerAddress = await prisma.partner_address.create({
+        data: {
+            street: "Av. Vicente Ferreira",
+            number: "45",
+            neighborhood: "Jardim Maria Izabel",
+            city: "Marília",
+            state: "SP",
+            zipCode: "17509-180",
+            longitude: -22.220058009980953,
+            latitude: -49.940245774540806
+        }
+    });
+
+    const partner = await prisma.partners.create({
+        data: {
+            name: "El Juanito",
+            logoUrl: "https://example.com/logo.png",
+            address_id: partnerAddress.id
+        }
+    });
+    console.log(partner);
 }
 
 main()
@@ -19,4 +42,5 @@ main()
 
 
 //    "prisma-migration": "npx prisma migrate dev --name nome_da_alteracao",
-//     "prisma-generate": "npx prisma generate"
+//    "prisma-generate": "npx prisma generate"
+//     run "npx tsx index.ts"
